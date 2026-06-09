67d7a44dfd19b226d54783c681a7de316533ad54
Jake Pagan <jacob.pagan10@gmail.com>
2026-06-09 18:46:37 -0500

    ...

--- include/orderbook/OrderBookManager.h
+++ include/orderbook/OrderBookManager.h
@@ -1,10 +1,11 @@
+#pragma once
 
 #include "orderbook/OrderBook.h"
 #include <unordered_map>
 
 namespace orderbook_manager {
 class OrderBookManager {
-
+  public:
     explicit OrderBookManager(const std::string &symbol)
         : _orderbooks({{symbol, orderbook::OrderBook(symbol)}}) {}
 
